42dc691ebe8c0ca511f1278845ea73588d5cb7d6
Mark Sample <samplereality@gmail.com>
2022-07-16 21:02:14 -0400

    more preps

--- grammar.js
+++ grammar.js
@@ -3,8 +3,8 @@ var rules = {
     ver1: ".adjective() $topic $actions $preps $topic .adverb()",
     ver2: "$stock .adjective() .pluralNoun() $actions $preps .noun()",
     ver3: ".adjective() .noun() .noun() $actions $preps .noun()",
-    preps: "ofthe | tothe | inthe | bythe | sothe",
-    stock: "ifall | every | ifany | wasnt | could | would | whose",
+    preps: "ofthe | tothe | inthe | bythe | onthe | andto",
+    stock: "ifall | every | ifany | wasnt | could | would | whose | sothe",
     $topic: ".noun()",
     actions: ".pastVerb() | .presentVerb() | .participleVerb()"
   };
